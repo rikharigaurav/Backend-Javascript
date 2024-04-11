90a4ed0757c0fbb4e8d8b1bf7b764d6bcad5e0b2
rikharigaurav <141213738+rikharigaurav@users.noreply.github.com>
2024-04-11 09:23:08 +0530

    Completed Controllers for Like and Playlist

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -7,23 +7,190 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
+    if(!isValidObjectId(videoId)){
+        throw new ApiErrors(
+            400,
+            "Invalid Video Id"
+        )
+    }
+
+    const VideoAlreadyLiked = await Like.findOne({
+        video: videoId,
+        likedBy: req.user?._id
+    })
+
+    if(VideoAlreadyLiked){
+        await Like.findByIdAndDelete(VideoAlreadyLiked._id);
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                201,
+                "Channel UnLiked"
+            )
+        )
+    } else {
+        await Like.create({
+            video: videoId,
+            likedBy: req.user?._id
+        })
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            "Video Liked Successfully"
+        )
+    )
+
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
+    if(!isValidObjectId(commentId)){
+        throw new ApiErrors(
+            400,
+            "Invalid Comment Id"
+        )
+    }
+
+    const commentLiked = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user?._id
+    })
+
+    if(commentLiked){
+        await Like.findByIdAndDelete(commentLiked._id)
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                "Comment Unliked Successfully"
+            )
+        )
+    } else {
+        await Like.create({
+            comment: commentId,
+            likedBy: req.user?._id
+        })
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            201,
+            "Comment Liked Successfully"
+        )
+    )
 
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
+
+    if(!isValidObjectId(tweetId)){
+        throw new ApiErrors(
+            400,
+            "Invalid Tweet Id"
+        )
+    }
+
+    const tweetLiked = await Like.findOne({
+        likedBy: req.user?._id,
+        tweet: tweetId
+    })
+
+    if(tweetLiked){
+        await Like.findByIdAndDelete(tweetLiked._id)
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                "Tweet Unliked Successfully"
+            )
+        )
+    } else {
+        await Like.create({
+            likedBy: req.user?._id,
+            tweet: tweetId
+        })
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            201,
+            "Tweet Liked Successfully"
+        )
+    )
+
 }
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
-})
+
+    const LikedVideos = await Like.aggregate([
+        {
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(req.user?._id),
+                video: {
+                    $exists: true
+                }
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "likedVideo"
+            }
+        },
+        {
+            $project: {
+                LikedVideo: {
+                    _id: 1,
+                    title: 1,
+                    description: 1,
+                    views: 1,
+                    owner: 1
+                }
+            }
+        }
+    ])
+
+    if(!LikedVideos?.length){
+        throw new ApiErrors(
+            400,
+            "User has no liked videos"
+        )
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            "User liked videos Fetched Successfully",
+            LikedVideos
+        )
+    )
+
+
+})  
+
 
 export {
     toggleCommentLike,
--- src/controllers/playlist.controllers.js
+++ src/controllers/playlist.controllers.js
@@ -8,40 +8,328 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description} = req.body
 
+    const { videoId } = req.params;
     //TODO: create playlist
+
+    if(!(name || description)){
+        throw new ApiErrors(
+            400,
+            "Playlist Name and Description requried"
+        )
+    }
+
+    if(!isValidObjectId(videoId)){
+        throw new ApiErrors(
+            400,
+            "Video not Available"
+        )
+    }
+
+
+    if(videoId){
+        const playlist = await Playlist.create({
+            name,
+            description,
+            video: videoId,
+            owner: req.user._id
+        })   
+    } else{
+        const playlist = await Playlist.create({
+            name,
+            description,
+            owner: req.user?._id
+    });
+    }
+
+    if(!playlist){
+        throw new ApiErrors(
+            400,
+            "Playlist Not Created"
+        )
+    }
+
+
+    return res
+    .status(200)
+    .json(
+        200,
+        playlist,
+        "Playlist Created Successully"
+    )
+
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
-    const {userId} = req.params
+    const { userId } = req.params
     //TODO: get user playlists
+    if(!isValidObjectId(userId)){
+        throw new ApiErrors(
+            400,
+            "UserID not valid"
+        )
+    }
+
+    const userPlaylist = await Playlist.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId( req.user._id )
+            }
+        }
+    ])
+
+    if (playlists.length === 0) {
+        return res
+            .status(404)
+            .json(new ApiResponse(400, playlists, "User has no playlists."));
+    }
+
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200, 
+                playlists, 
+                "Playlists fetched successfully."
+            )
+    );
+
+
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
+    const { playlistId } = req.params
     //TODO: get playlist by id
+
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiErrors(400, "Invalid playlist");
+    }
+
+    const playlist = await Playlist.findbyId(playlistId);
+
+    if(!playlistId){
+        throw new ApiErrors(
+            400,
+            "Playlist Not found"
+        )
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200, 
+                playlists, 
+                "Playlists fetched successfully."
+            )
+    );
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
+
+    if(!(isValidObjectId(playlistId) && isValidObjectId(videoId))){
+        throw new ApiErrors(
+            400,
+            "Invalid Playlist or video"
+        )
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+    const video = await Video.findById(videoId);
+
+    if (!playlist) {
+        throw new ApiErrors(
+            400, 
+            "Playlist not found"
+        );
+    }
+
+    if (!video) {
+        throw new ApiErrors(
+            400, 
+            "Video not found"
+        );
+    }
+ 
+    let addvideo;
+    if(playlist.owner.toString() === req.user._is.toString()){
+        addvideo = await Playlist.findByIdAndUpdate(
+            playlistId, 
+            {
+                $addToSet: {
+                    videos: videoId,
+                },
+            }, { new: true }
+        );
+    } else {
+        throw new ApiErrors(
+            400,
+            "Unauthorized access to playlist"
+        )
+    }
+
+    if(!addvideo){
+        throw new ApiErrors(
+            400,
+            "Something went wrong"
+        )
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            "Video Added Successfully"
+        )
+    )
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
     // TODO: remove video from playlist
 
+    if (!(isValidObjectId(playlistId) && isValidObjectId(videoId))) {
+        throw new ApiErrors(400, "Invalid Playlist or video");
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+    const video = await Video.findById(videoId);
+
+    if (!playlist) {
+        throw new ApiErrors(400, "Playlist not found");
+    }
+
+    if (!video) {
+        throw new ApiErrors(400, "Video not found");
+    }
+
+    let deleteVideo;
+    if (playlist.owner.toString() === req.user._is.toString()) {
+        deleteVideo = await Playlist.findByIdAndUpdate(
+            playlistId,
+            {
+                $pull: {
+                    videos: videoId,
+                },
+            },
+            { new: true }
+        );
+    } else {
+        throw new ApiErrors(
+            400, 
+            "Unauthorized access to playlist"
+        );
+    }
+
+    if(!deleteVideo){
+        throw new ApiErrors(
+            400,
+            "Couldn't Delete Video. Try Again"
+        )
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            "Video removed from playlist"
+        )
+    )
+
+
 })
 
 const deletePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     // TODO: delete playlist
+
+    if(!isValidObjectId(playlistId)){
+        throw new ApiErrors(
+            400,
+            "Invalid Playlist ID"
+        )
+    }
+
+    const deletePlaylist = await Playlist.findById(
+        playlistId
+    )
+
+    if(deletePlaylist.owner.toString() === req.user._id.toString()){
+        await Playlist.findByIdAndDelete(
+            playlistId
+        )
+    } else {
+        throw new ApiErrors(
+            400,
+            "Unauthorized access to playlist"
+        )
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            "Playlist Deleted Successfully"
+        )
+    )
+
 })
 
 const updatePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     const {name, description} = req.body
     //TODO: update playlist
-})
 
+    if(!isValidObjectId(playlistId)){
+        throw new ApiErrors(
+            400,
+            "Invalid Playlist ID"
+        )
+    }
+
+    if(name.trim() === "" || description.trim() === ""){
+        throw new ApiErrors(
+            400,
+            "Name or Description required to Update"
+        )
+    }
+
+    const update = await Playlist.findById(playlistId);
+
+    if(!playlistId){
+        throw new ApiErrors(
+            400,
+            "No playlist found"
+        )
+    }
+
+    if(update.owner.toString() === req.user._if.toString()){
+        await playlist.findByIdAndUpdate(
+            playlistId,
+            {
+                $set: {
+                    name: name,
+                    description: description
+                }
+            }, { new: true }
+        )
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            update,
+            "Playlist Upadated Successfully"
+        )
+    )
+})
+ 
 export {
     createPlaylist,
     getUserPlaylists,
