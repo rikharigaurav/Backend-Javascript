06ae4a9341aea0c300389b5277a996fdfc68a861
rikharigaurav <141213738+rikharigaurav@users.noreply.github.com>
2024-04-11 10:49:52 +0530

    Controllers Completed

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -1,8 +1,9 @@
-import mongoose from "mongoose"
+import mongoose, { isValidObjectId } from "mongoose"
 import {Comment} from "../models/comment.model.js"
 import {ApiErrors} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js"
 
 
 const getVideoComments = asyncHandler(async (req, res) => {
@@ -10,21 +11,115 @@ const getVideoComments = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
 
+    if(!isValidObjectId(videoId)){
+        throw new ApiErrors(
+            400,
+            "Invalid video Id"
+        )
+    }
+
+    const video = await Video.findById(videoId);
+
+    if(!video){
+        throw new ApiErrors(
+            400,
+            "Video Not Found"
+        )
+    }
+
+    const comment = await Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId)
+            }
+        }, 
+        {
+            $lookup: {
+                from: "user",
+                localField: "owner",
+                foreignField: "_id",
+                as: "commentOwner"
+            }
+        }, {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "comment",
+                as: "commentlikes"
+            }
+        },
+        {
+            $project: {
+                content: 1,
+                createdAt: 1,
+            }
+        }
+    ])
+
+    if(!comment){
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                comment,
+                "Video Comment Fetched Successfully"
+            )
+        )
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            comment,
+            "Video Comment Fetched Successfully"
+        )
+    )
+
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video[]
-    const { content, video, owner } = req.body
+    const { content } = req.body
+    const { videoId } = req.params
 
-    if([content, video, owner].some((field) => field?.trim() === "")){
-        throw new ApiError(400, "All fields are required")
+    if (content === "") {
+        throw new ApiErrors(
+            400, 
+            "Content is required."
+        );
+    }
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiErrors(
+            400, 
+            "Invalid videoId"
+        );
+    }
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiErrors(
+            400, 
+            "Video not found"
+        );
     }
 
     const comment = await Comment.create({
         content,
-        video, 
-        owner
+        video: video._id, 
+        owner: req.user?._id
     })
+
+    if(!comment){
+        throw new ApiErrors(
+            400,
+            "Coudn't create Comment"
+        )
+    }
 
     return res
     .status(201)
@@ -40,13 +135,25 @@ const addComment = asyncHandler(async (req, res) => {
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
     const { content } = req.body
+    const { commentId } = req.params;
 
-    if(!content) {
-        throw new ApiError(400, "All fields are required")
+    if(!isValidObjectId(commentId)) {
+        throw new ApiErrors(
+            400, 
+            "Invalid commentId"
+        );
     }
 
-    const commment = await Comment.findByIdAndUpdate(
-        req.comment?._id, 
+
+    if(!content) {
+        throw new ApiErrors(
+            400, 
+            "Content is required"
+        )
+    }
+
+    const comment = await Comment.findByIdAndUpdate(
+        commentId, 
         {
             $set: {
                 content: content,
@@ -69,8 +176,25 @@ const updateComment = asyncHandler(async (req, res) => {
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
-    await Comment.findByIdAndDelete(
-        req.user._id,
+
+    const {commentId} = req.params;
+
+    if (!isValidObjectId(commentId)) {
+      throw new ApiErrors(
+        400, 
+        "Invalid commentId"
+    );
+    }
+
+    await Comment.findByIdAndDelete(commentId)
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            "Comment Deleted Successfully"
+        )
     )
 })
 
--- src/controllers/dashboard.controllers.js
+++ src/controllers/dashboard.controllers.js
@@ -2,16 +2,135 @@ import mongoose from "mongoose"
 import {Video} from "../models/video.model.js"
 import {Subscription} from "../models/subscription.model.js"
 import {Like} from "../models/like.model.js"
-import { ApiErrors } from "../utils/ApiError.js";
+import { ApiErrors } from "../utils/ApiError.js"
+import {User} from "../models/user.model.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+
+    const channel = await User.findById(req.user?.id)
+
+    if(!channel){
+        throw new ApiErrors(
+            400,
+            "Channel Not Found"
+        )
+    }
+
+    const Stats = await Video.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(channel._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "video",
+                as: "video_likes"
+            }
+        }, 
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "owner",
+                foreignField: " channel",
+                as: "total_subscribers"
+            }
+        },
+        {
+            $group: {
+                TotalView: {
+                    $sum: "$views"
+                },
+                TotalVideo: {
+                    $sum: 1
+                },
+                TotalSubscribers: {
+                    $first: {
+                        $size: "$total_subscribers"
+                    }
+                },
+                TotalLikes: {
+                    $first: {
+                        $size: "$video_likes"
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                TotalVideos: 1,
+                TotalSubscribers: 1,
+                TotalViews: 1,
+                TotalLikes: 1,
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            Stats,
+            "Channel Stats Fetched Successfully"
+        )
+    )
+
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+
+    const channel = await User.findById(req.user?._id)
+
+    if(!channel){
+        throw new ApiErrors(
+            400,
+            "Channel Not Found"
+        )
+    }
+
+    const channelVideo = await Video.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(channel._id)
+            }
+        },
+        {
+            $project: {
+                _id: 1,
+                title: 1,
+                description: 1,
+                videoFile: 1,
+                thumbnail: 1,
+                createdAt: 1,
+            }
+        }
+    ])
+
+    if(!channelVideo.length){
+        throw new ApiErrors(
+            400,
+            "No videos found"
+        )
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            channelVideo,
+            "Channel Video Fetched Successfully"
+        )
+    )
+
+
 })
 
 export {
